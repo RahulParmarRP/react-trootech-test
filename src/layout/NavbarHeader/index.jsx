25c8e8f370a4fd32de079cc94aa86d83bbee3200
Rahul Parmar <rahulparmar.n@gmail.com>
2021-11-28 21:58:05 +0530

    Update index.jsx

--- src/layout/NavbarHeader/index.jsx
+++ src/layout/NavbarHeader/index.jsx
@@ -5,15 +5,8 @@ const NavbarHeader = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">
-                    <img
-                        alt=""
-                        src="/logo.svg"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />{' '}
-                    Todo List
+                <Navbar.Brand href="/">
+                    Todo App
                 </Navbar.Brand>
             </Container>
         </Navbar>
